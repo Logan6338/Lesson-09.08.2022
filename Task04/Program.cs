b5baa76f10f8c544d58d35335b15cd5ee98b003d
Dmitry <Logan6338@mail.ru>
2022-08-09 22:10:17 +0300

    Задача 4

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+void Delat()
+{
+    if ((number % 7 == 0) & (number % 23 == 0))
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        Console.WriteLine("Не кратное");
+    }
+}
+
+Delat();
