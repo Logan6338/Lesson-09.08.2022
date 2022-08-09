21267bb58380c0aed00c49cb28ff413dce98b99e
Dmitry <Logan6338@mail.ru>
2022-08-09 20:50:46 +0300

    Задание 1

---
+++ Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿int number = GetMaxNumberRandomValue();
+
+Console.WriteLine(number);
+
+int GetMaxNumberRandomValue()
+{
+
+    int randomValue = new Random().Next(10, 100);
+
+    Console.WriteLine(randomValue);
+
+    int LastNumber = randomValue % 10;
+    int FirstNumber = randomValue / 10;
+
+    int max = LastNumber;
+    if (LastNumber == FirstNumber)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else
+    {
+
+        if (max < FirstNumber)
+        {
+            max = FirstNumber;
+        }
+        Console.WriteLine($"max = {max}");
+    }
+       return new Random().Next(10, 100);
+}
+
