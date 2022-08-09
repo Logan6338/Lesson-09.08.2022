790f174316ea2b6e9b6c589c58774a6f9d75f979
Dmitry <Logan6338@mail.ru>
2022-08-09 21:46:17 +0300

    Задача 3

---
+++ Task03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+void multiple()
+{
+    if ((numberA % numberB == 0) & (numberB % numberA ==0))
+    {
+        Console.WriteLine("Числа кратные");
+    }
+    else
+    {
+        Console.WriteLine($"Остаток - {numberA % numberB}!");
+    }
+}
+multiple();
