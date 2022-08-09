9b831695152ac3c6ad4a88dd5c4e8dde292e1264
Dmitry <Logan6338@mail.ru>
2022-08-09 21:08:59 +0300

    Задача 2

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = Convert.ToInt32(new Random().Next(100, 1000));
+
+void find()
+{
+    Console.WriteLine(number);
+    Console.WriteLine();
+    Console.Write(number / 100);
+    Console.Write(number % 10);
+}
+
+find();
